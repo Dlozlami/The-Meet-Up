ff3f0c534b516170795759c5ba207abf772f12df
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-07-06 08:39:29 +0200

    EXPO REDUX INSTALL

---
+++ app/loginPage.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+
+export default function LoginPage()
+{
+    return (
+        <ScrollView>
+          <Text style={{ margin: 10 }}>Hello World</Text>
+          <View style={{ marginTop: 512 }} />
+          <Text style={{ margin: 10 }}>Welcome to React Native</Text>
+        </ScrollView>
+      );
+}
