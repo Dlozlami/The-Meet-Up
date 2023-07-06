c9bfa08a374e94d5f3847833241a3d1833f27ae8
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-07-06 13:09:43 +0200

    no double tab

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,54 @@
-import { Stack } from 'expo-router';
+import React from 'react';
+import { Tabs } from 'expo-router/tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function HomeLayout() {
-    return (
-        <Stack />
-    )
+export default function AppLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false, // Hide tab labels
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+            title:'Home | The Meet Up',
+            headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="login"
+        options={{
+            title:'Log in | The Meet Up',
+            headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="account" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="signup"
+        options={{
+            title:'Register | The Meet Up',
+            headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="clipboard-edit-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="schedule"
+        options={{
+            title:'Event Programme | The Meet Up',
+            headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="calendar" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
---
+++ app/schedule.js
@@ -0,0 +1,12 @@
+import { Text, View, ScrollView } from "react-native";
+
+export default function Schedule()
+{
+    return (
+        <ScrollView>
+          <Text style={{ margin: 10 }}>Schedule</Text>
+          <View style={{ marginTop: 512 }} />
+          <Text style={{ margin: 10 }}>Welcome to React Native</Text>
+        </ScrollView>
+      );
+}
