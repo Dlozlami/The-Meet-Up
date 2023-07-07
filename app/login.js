00de2f79832dd9a16afdb1285f4a801b5ff2ee4f
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-07-07 09:16:53 +0200

    push

--- app/login.js
+++ app/login.js
@@ -2,12 +2,13 @@ import { ImageBackground,Text, View, ScrollView } from "react-native";
 
 export default function Login()
 {
-    return (<ImageBackground source={require('../assets/images/bg.jpg')}  style={{width: '100%', height: '100%'}}>
+    return (
+      <ImageBackground source={require('../assets/images/bg.jpg')}  style={{width: '100%', height: '100%'}}>
         <ScrollView >
           <Text style={{ margin: 10 }}>Hello World</Text>
           <View style={{ marginTop: 512 }} />
           <Text style={{ margin: 10 }}>log in</Text>
         </ScrollView>
-        </ImageBackground>
+      </ImageBackground>
         );
 }
